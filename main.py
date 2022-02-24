e868b687c66e10e4f0f1c537071fb607dab9c2b7
hamed <hamedremy @hotmail.fr>
2022-02-24 22:07:01 +0100

    maj

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
 
+
 def current_milli_time():
     return round(time.time() * 1000)
 
+
 class Cell:
 
-    #constructeur
+    # constructeur
     def __init__(self, value):
-        self.is_alive = value
+        self._is_alive = value
 
     @property
     def is_alive(self):
@@ -24,8 +26,7 @@ class Cell:
         else:
             return value
 
-
-    def process_state(self,is_alive, nb_neighbour_cells_alive):
+    def process_state(self, is_alive, nb_neighbour_cells_alive):
 
         if is_alive and (nb_neighbour_cells_alive == 2 or nb_neighbour_cells_alive == 3):
             return True
@@ -37,20 +38,21 @@ class Cell:
 
     def to_string(self):
         if self.is_alive:
-            return  " X"
+            return " X"
         else:
             return " ."
 
 
-
-
 class Grid:
 
-    #constructeur
+    # constructeur
     def __init__(self, square_size):
         self._square_size = square_size
-        self._cells = [[Cell(False)]* self.square_size for i in range( self.square_size)]
-        self._temps_cells = [[Cell(False)]* self.square_size for i in range( self.square_size)]
+        for i in range(0, self.square_size):
+            self._cells = [[Cell(False)] * self.square_size for i in range(self.square_size)]
+        for i in range(0, self.square_size):
+            self._temps_cells = [[Cell(False)] * self.square_size for i in range(self.square_size)]
+        self._display = " "
 
     @property
     def square_size(self):
@@ -70,18 +72,16 @@ class Grid:
             raise AttributeError(f"square_size = {value} is under 18")
 
     def initial_cells(self):
-        #for i in range(0, self.square_size):
+        # for i in range(0, self.square_size):
         #    self.cells = [[Cell(False)]* self.square_size for i in range( self.square_size)]
         #    self.temps_cells.append([Cell(False) for x in range(0, self.square_size)])
 
-        for i in range(0, self._square_size ):
-            for j in range(0, self._square_size ):
-                if current_milli_time() % 2 == 0 :
+        for i in range(0, self._square_size):
+            for j in range(0, self._square_size):
+                if current_milli_time() % 2 == 0:
                     self.cells[i][j] = Cell(False)
                 else:
                     self.cells[i][j] = Cell(True)
-
-
 
     def generate_next_state(self):
         count = 0
@@ -93,24 +93,27 @@ class Grid:
                             continue
                         if k < 0 or l < 0:
                             continue
-                        if k >= len(self.cells) or 1 >= len(self.cells):
+                        if k >= len(self.cells) or l >= len(self.cells):
                             continue
                         if self.cells[k][l].is_alive:
                             count += 1
-                print(m, j)
+
                 if self.cells[m][j].process_state(self.cells[m][j].is_alive, count):
                     self.temps_cells[m][j].is_alive = True
                 else:
                     self.temps_cells[m][j].is_alive = False
                 count = 0
-        self.cells = self.temps_cells
+        self.cells = self.temps_cells.copy()
+
+        new_string_display = ""
         for m in range(0, len(self.cells)):
             for j in range(0, len(self.cells)):
-                self.display = self.display + self.cells[m][j].to_string()
+                self.display(new_string_display + self.cells[m][j].__str__)
                 if j < (len(self.cells) - 1):
-                    display = self.display + " "
+                    new_string_display = new_string_display + " "
                 if j == (len(self.cells) - 1) and m < (len(self.cells) - 1):
-                    display = self.display + "\n"
+                    new_string_display = new_string_display + "\n"
+        self.display(new_string_display)
 
 
     @property
@@ -118,18 +121,18 @@ class Grid:
         return self._cells
 
     @cells.setter
-    def cells(self, tab):
-        self._cells = self.__check_cells(tab)
+    def cells(self, string):
+        self._cells = string
 
-    def __check_cells(self, tab):
-        res = isinstance(tab, list)
-        if not res:
-            raise AttributeError(f"tab = {tab} is not a list")
-        else:
-            for cell in tab:
-                res = isinstance(cell, Cell)
-                if not res:
-                    raise AttributeError(f"tab = {tab} need to contain cells only")
+    # def __check_cells(self, tab):
+    #    res = isinstance(tab, list)
+    #    if not res:
+    #       raise AttributeError(f"tab = {tab} is not a list")
+    # else:
+    #   for cell in tab:
+    #      res = isinstance(cell, Cell)
+    #     if not res:
+    #        raise AttributeError(f"tab = {tab} need to contain cells only")
 
     @property
     def temps_cells(self):
@@ -137,30 +140,27 @@ class Grid:
 
     @temps_cells.setter
     def temps_cells(self, tab):
-        self._temps_cells = self.__check_cells(tab)
+        self._temps_cells = tab
 
     @property
     def display(self):
         return self._display
 
     @display.setter
-    def display(self, tab):
-        self._display = self.__check_display(tab)
+    def display(self, string):
+        self._display = string
 
-    def __check_display(self, string):
-        res = isinstance(string, str)
-        if not res:
-            raise AttributeError(f"string = {string} is not a string")
-        #else:
-
+   # def __check_display(self, string):
+   #     res = isinstance(string, str)
+   #     if not res:
+   #         raise AttributeError(f"string = {string} is not a string")
 
 
-g = Grid( 20)
+g = Grid(20)
 print(g.square_size)
 g.initial_cells()
+print(g.display)
 for i in range(0, 50):
     g.generate_next_state()
     print(g.display)
     print("\n\n\n")
-
-
