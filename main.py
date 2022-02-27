c3f82966bf99cbfa07c2fa210a0eb2c2cb42ed7f
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-02-27 22:17:07 +0100

    add File loading

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import time
+import ast
+from tkinter import filedialog as fd
 import random
 import tkinter
 from tkinter import *
-
-def current_milli_time():
-    return round(time.time() * 1000)
-
 
 class Cell:
 
@@ -43,19 +40,30 @@ class Cell:
 
 
 class Grid:
-
     # constructeur
     def __init__(self, square_size):
         self._square_size = square_size
-        self._cells = [[]]
-        self._cells.clear()
-        for i in range(0, self._square_size):
-            self._cells.append([])
-            for j in range(0, self._square_size):
-                if random.randint(1, 10000) < 5000:
-                    self._cells[i].append(Cell(False))
-                else:
-                    self._cells[i].append((Cell(True)))
+        if is_file_needed:
+            temp_grid = list(file_grid)
+            self._cells = []
+            self._cells.clear()
+            for i in range(self._square_size):
+                self._cells.append([])
+                for j in range(self._square_size):
+                    if temp_grid[i][j] == 'X':
+                        self._cells[i].append(Cell(True))
+                    if temp_grid[i][j] == 'O':
+                        self._cells[i].append(Cell(False))
+        else:
+            self._cells = []
+            self._cells.clear()
+            for i in range(0, self._square_size):
+                self._cells.append([])
+                for j in range(0, self._square_size):
+                    if random.randint(1, 10000) < 5000:
+                        self._cells[i].append(Cell(False))
+                    else:
+                        self._cells[i].append((Cell(True)))
         self._display = " "
 
     @property
@@ -70,12 +78,12 @@ class Grid:
         try:
             value = int(value)
             if value < 18:
-                raise AttributeError(f"square_size = {value} is under 18")
+                raise ValueError()
             return value
-        except Exception:
+        except AttributeError:
             raise AttributeError(f"square_size = {value} is not convertible to integer")
-
-
+        except ValueError:
+            raise ValueError(f"square_size = {value} is under 18")
     #def initial_cells(self):
         # for i in range(0, self.square_size):
         #    self.cells = [[Cell(False)]* self.square_size for i in range( self.square_size)]
@@ -97,7 +105,8 @@ class Grid:
 
     def generate_next_state(self):
         count = 0
-        nwtab = [[]]
+        nwtab = []
+        nwtab.clear()
         for m in range(0, self.square_size): # 0
             nwtab.append([])
             for j in range(0, self.square_size): # 0
@@ -112,15 +121,9 @@ class Grid:
                         if self.cells[k][l].is_alive:
                             count += 1
                 if self.cells[m][j].process_state(self.cells[m][j].is_alive, count):
-                    #print("vrai")
-                    #self.temps_cells[m][j].set_alive(True) #is_alive(True)
                     nwtab[m].append(Cell(True))
-                    #print(self.temps_cells[m][j].is_alive)
                 else:
-                    #print("false")
-                    #self.temps_cells[m][j].set_alive(False) #is_alive(True)
                     nwtab[m].append(Cell(False))
-                    #print(self.temps_cells[m][j].is_alive)
                 count = 0
         
         self.cells = nwtab
@@ -159,82 +162,97 @@ class Grid:
    #         raise AttributeError(f"string = {string} is not a string")
 
 
-g = Grid(25)
-print(g.square_size)
-'''
-print("1\n")
-print("2\n")
-g.show()
-
-print("3\n\n")
-g.generate_next_state()
-g.show()
-print("4\n\n")
-g.generate_next_state()
-g.show()
-
-for i in range(0, 50):
-    g.generate_next_state()
-    g.show()
-    print("-------------\n")
-
-'''
-
-# création de la fenetre
-window = Tk()
-window.title("Game Of Life") # titre
-window.geometry("1080x720") # taille
-window.minsize(1920, 1080) # taille minimal
-window.iconbitmap("logo.ico") #logo (.ico obligatoire)
-window.config(background="grey") # couleur de fond
-
-#creation d'un composant
-title = Label(window, text="Game Of Life", font=("Arial", 40), bg="white", fg="red")
-title.pack() # ajoute le composant a la fenetre
-
-#creation d'un second composant
-subtitle = Label(window, text="Sa veut dire jeu de la vie en français !", font=("Arial", 10), bg="white", fg="red")
-subtitle.pack()
-
-#creation d'un composant qui va contenir notre grille
-grid = Frame(window, bg="grey")
-grid.pack()
-# affichage de la grille
-
+is_file_needed = False
+file_grid = []
+g = Grid(20)
 canvas_list = [[]]
 
-def init_grid():
-    canvas_list.clear()
-    g.generate_next_state()
-    for i in range(0, g.square_size):
-        canvas_list.append([])
-        for j in range(0, g.square_size):
-            if(g.cells[i][j].is_alive):
-                chosencolor = "black"
-            else:
-                chosencolor = "white"
-            # canvas qui va nous servire a dessiner notre couleurs
-            canvas = Canvas(grid, width=25, height=25, bg=chosencolor, highlightthickness=0)
-            canvas.grid(row=i, column=j)
-            canvas_list[i].append(canvas)
+class Window(tkinter.Tk):
+    def __init__(self, *args, **kwargs):
+        tkinter.Tk.__init__(self, *args, **kwargs)
+        self.title("Game Of Life")  # titre
+        self.geometry("1080x720")  # taille
+        self.minsize(500, 300)  # taille minimal
+        self.iconbitmap("logo.ico")  # logo (.ico obligatoire)
+        self.config(background="grey")  # couleur de fond
+        title = Label(self, text="Game Of Life", font=("Arial", 40), bg="grey", fg="white")
+        title.pack()  # ajoute le composant a la fenetre
+        self.container = tkinter.Frame(self)
+        self.container.pack()
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        self.show_main_frame()
 
-def refresh_grid():
-    g.generate_next_state()
-    for k in range(g.square_size):
-        for i in range(g.square_size):
-            if (g.cells[k][i].is_alive):
-                chosencolor = "black"
-            else:
-                chosencolor = "white"
-            canvas = canvas_list[k][i]
-            canvas.configure(bg=chosencolor)
-            canvas_list[k][i] = canvas
-    grid.after(100, refresh_grid)
-
-init_grid()
-refresh_grid()
-window.mainloop()
-
+    def load_file(self):
+        global file_grid
+        global is_file_needed
+        global g
+        is_file_needed = True
+        filename = fd.askopenfilename()
+        filin = open(filename, "r")
+        file_grid = (ast.literal_eval(s) for s in filin)
+        file_grid = list(file_grid)
+        cpt = 0
+        for l in file_grid:
+            cpt = cpt + 1
+        g = Grid(cpt)
+        self.show_grid_frame()
 
 
+    def show_main_frame(self):
+        frame = Main_frame(parent=self.container, controller=self)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.tkraise()
 
+    def show_grid_frame(self):
+        frame = Grid_frame(parent=self.container, controller=self)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.tkraise()
+
+class Main_frame(tkinter.Frame):
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+        self.controller = controller
+        btn_load = tkinter.Button(self, text="Charger", command=lambda: controller.load_file())
+        btn_load.pack()
+        btn_generate = tkinter.Button(self, text="Générer", command=lambda: controller.show_grid_frame())
+        btn_generate.pack()
+
+
+class Grid_frame(tkinter.Frame):
+    def __init__(self, parent, controller):
+        tkinter.Frame.__init__(self, parent)
+        self.controller = controller
+        self.create_grid()
+        self.refresh_grid()
+
+    def create_grid(self):
+        canvas_list.clear()
+        g.generate_next_state()
+        for i in range(0, g.square_size):
+            canvas_list.append([])
+            for j in range(0, g.square_size):
+                if (g.cells[i][j].is_alive):
+                    chosencolor = "black"
+                else:
+                    chosencolor = "white"
+                # canvas qui va nous servire a dessiner notre couleurs
+                canvas = Canvas(self, width=25, height=25, bg=chosencolor, highlightthickness=0)
+                canvas.grid(row=i, column=j)
+                canvas_list[i].append(canvas)
+
+    def refresh_grid(self):
+        g.generate_next_state()
+        for k in range(g.square_size):
+            for i in range(g.square_size):
+                if (g.cells[k][i].is_alive):
+                    chosencolor = "black"
+                else:
+                    chosencolor = "white"
+                canvas = canvas_list[k][i]
+                canvas.configure(bg=chosencolor)
+                canvas_list[k][i] = canvas
+        self.after(100, self.refresh_grid)
+
+app = Window()
+app.mainloop()
